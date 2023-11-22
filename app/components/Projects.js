78772041563605d3d296a083e82eb7c668a6084f
Jerry <jca476@sfu.ca>
2023-11-22 00:14:31 -0800

    content update 4

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Typography, Box, List, ListItem, ListItemText, Divider, Link } from '@mui/material'
+import { Typography, Box, List, ListItem, ListItemText, ListItemAvatar, Avatar, Divider, Link } from '@mui/material'
 
 export default function Projects() {
 
     return (
-        <Box>
+        <Box
+            padding={2}
+        >
             <Typography variant="h5">
                 Projects
             </Typography>
+
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                 <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar alt="LOGO" src="/NEXT_LOGO.png" />
+                    </ListItemAvatar>
                     <ListItemText
                         primary="Website"
                         secondary={
@@ -29,7 +35,11 @@ export default function Projects() {
                         }
                     />
                 </ListItem>
-                <Divider variant="inset" component="li" />                <ListItem alignItems="flex-start">
+                <Divider variant="inset" component="li" />
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar alt="LOGO" src="/FLIGHTRADAR24_LOGO.png" />
+                    </ListItemAvatar>
                     <ListItemText
                         primary="Flight Radar "
                         secondary={
@@ -50,7 +60,30 @@ export default function Projects() {
                     />
                 </ListItem>
                 <Divider variant="inset" component="li" />
-                
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar alt="LOGO" src="/COREAPI_LOGO.png" />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary="Scholar Sprint"
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    <Link href="https://github.com/the-pirated-sardar/1Sky-Project-276" underline="none">
+                                        {"Github"}
+                                    </Link>
+                                </Typography>
+                                {" - React project with search and summarization feautures for academic papers. Makes calls to Core API for resources and implements GPT API for summary features."}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItem>
+                <Divider variant="inset" component="li" />
             </List>
         </Box>
     )
