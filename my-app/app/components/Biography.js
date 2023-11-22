c09540c8d0b0e874acbd3566910b4f75e44ab55c
Jerry <jca476@sfu.ca>
2023-11-21 19:12:42 -0800

    issue #2

--- my-app/app/components/Biography.js
+++ my-app/app/components/Biography.js
@@ -1,30 +1,38 @@
-import React from "react";
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Typography, Box, Paper, Grid } from '@mui/material';
 
-const Biography = () => {
+const BioSection = () => {
+  return (
+    <Box p={3}>
+      <Typography variant="h4" gutterBottom>
+        My Bio
+      </Typography>
+      <Grid container spacing={2}>
+        {/* Bio/Overview Section */}
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={3} style={{ padding: '20px' }}>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat.
+            </Typography>
+          </Paper>
+        </Grid>
 
-    return (
-        <Box>
-            <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                height="25vh"
-            >
-                <Typography
-                    variant="h4"
-                >
-                    Hello World!
-                </Typography>
-                <Typography
-                    variant="p"
-                >
-                    I'm Jerry. I study, code, and play games.
-                </Typography>
-            </Box>
-        </Box>
+        {/* Image Block */}
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={3}>
+            {/* Replace the image URL with your own */}
+            <img
+              src="https://via.placeholder.com/400x400"
+              alt="Profile"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-    )
-}
-
-export default Biography;
+export default BioSection;
