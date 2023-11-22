b1ccf4c4db77f9034ff81ce7ad7d5a133d92f9b5
Jerry <jca476@sfu.ca>
2023-11-21 23:46:29 -0800

    middle area content

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,29 +1,24 @@
 import React from 'react'
-import { Typography, Box, Grid } from '@mui/material'
+import { Typography, Box, List, ListItemText } from '@mui/material'
 
 export default function Projects() {
 
     return (
         <Box>
-            <Grid>
-
-                <Typography>
-                    Projects
-                </Typography>
-                <Typography variant="body1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat.
-                </Typography>
-
-            </Grid>
-            <Grid>
-                <Typography variant="body1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat.
-                </Typography>
-            </Grid>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <ListItemText>
+                    Project 1
+                </ListItemText>
+                <ListItemText>
+                    Project 1
+                </ListItemText>
+                <ListItemText>
+                    Project 1
+                </ListItemText>
+                <ListItemText>
+                    Project 1
+                </ListItemText>
+            </List>
         </Box>
     )
 }
